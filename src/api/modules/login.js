e5fc3d1dc8b47c891885177002ae98d21597d72d
xuguangwen <326531916@qq.com>
2018-11-02 18:33:29 +0800

    封装请求

--- src/api/modules/login.js
+++ src/api/modules/login.js
@@ -1,4 +1,33 @@
 import axios from 'axios';
-export const login = params => axios.post('/m2_basicData/production/queryWorkDayAbstract.do', params);
-export const getInfo = params => axios.post('/m2_basicData/production/queryWorkDayAbstract.do', params);
-export const logout = params => axios.post('/m2_basicData/production/queryWorkDayAbstract.do', params);
+var instance = axios.create({
+    baseURL: 'https://some-domain.com/api/',
+    timeout: 1000,
+  });
+export function login(username, password) {
+    return instance({
+      url: '/user/login',
+      method: 'post',
+      data: {
+        username,
+        password
+      }
+    })
+  }
+  
+  export function getInfo(token) {
+    return instance({
+      url: '/user/info',
+      method: 'get',
+      params: { token }
+    })
+  }
+  
+  export function logout() {
+    return instance({
+      url: '/user/logout',
+      method: 'post'
+    })
+  }
+// export const login = params => axios.post('/m2_basicData/production/queryWorkDayAbstract.do', params);
+// export const getInfo = params => axios.post('/m2_basicData/production/queryWorkDayAbstract.do', params);
+// export const logout = params => axios.post('/m2_basicData/production/queryWorkDayAbstract.do', params);
