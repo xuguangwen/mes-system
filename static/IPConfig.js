c839df904e72f944f46533bcdb9cd9bb14b8a7cb
xuguangwen <326531916@qq.com>
2019-08-12 17:48:56 +0800

    视频说明文件

--- static/IPConfig.js
+++ static/IPConfig.js
@@ -1,8 +1,8 @@
 //1.定义所有环境服务器地址
 const server={
   pro:"localhost",//生产环境
-  dev:"http://www.imes-inc.ser",//开发环境
-  util:"http://www.imes-inc.ser"//其他环境，可以为空，为空则适配到测试环境
+  dev:"http://www.XXXXXXX",//开发环境
+  util:"http://www.XXXXXXX"//其他环境，可以为空，为空则适配到测试环境
 }
 //2.获取当前访问域名，并根据域名判断当前环境，然后获取指定环境的服务器地址
 var DOMIN="";//服务端接口访问跟路径
